7284e952055d7ea0d7c6a4dbcadf82059aa25935
Simon <12232@192.168.123.227>
2018-05-18 23:37:33 +0800

    666

---
+++ 002Consel/js/alert.js
@@ -0,0 +1,16 @@
+alert("你好像有点呆\n别点👇");
+alert("你还真的点了，果然不是一般的😳");
+alert("233");
+function wonder(){
+    alert("你猜一会儿会发生什么 ");
+    alert("别着急");
+    alert("再等等嘛~");
+}
+alert("你相信有奇迹嘛");
+alert("我提个问题");
+wonder();
+alert("果然没有奇迹呢！233");
+alert("我们就这样放弃了吗");
+wonder();
+alert("还是没有奇迹呢！");
+alert("这是最后一句了，拜拜~");
